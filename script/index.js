66b6b2d083d35e4d956591fb4da178fd0345720d
Hossain-Mahmud40 <hossainpromil6286@gmail.com>
2026-05-21 04:02:48 +0600

    Add word container and functionality to display level words

--- script/index.js
+++ script/index.js
@@ -3,6 +3,25 @@ const loadLessons = () => {
     .then((res) => res.json()) // promise of json data
     .then((json) => displayLessons(json.data));
 };
+
+const loadLevelWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data));
+};
+const displayLevelWord = (words) => {
+  const wordContainer = document.getElementById("word-container");
+  // wordContainer.innerHTML = "";
+  words.forEach((word) => {
+    const card = document.createElement("div");
+    card.innerHTML = `
+    <p>Cat</p>
+    `;
+    wordContainer.append(card);
+  });
+};
+
 const displayLessons = (lessons) => {
   // 1. get the container & empty
   const levelContainer = document.getElementById("level-container");
@@ -12,7 +31,7 @@ const displayLessons = (lessons) => {
     // 3. Create Element
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
-    <button class="btn btn-outline btn-primary">
+    <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
       <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}
     </button>
     `;
